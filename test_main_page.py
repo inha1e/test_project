fb244e0fafc1bbc8afdf5e7e40435f8c17e7d168
Roman R <butcher_100@mail.ru>
2022-03-27 23:44:24 +0300

    added login page test

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 import time
 
 def test_guest_can_go_to_login_page(browser):
@@ -7,4 +8,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    #page.go_to_login_page()
+    page.go_to_login_page()
+    
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+    
+    
